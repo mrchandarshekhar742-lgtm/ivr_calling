9d9ed40353a0e7e2e4ee44ca8ce580f0e8a5027f
mrchandarshekhar742-lgtm <mrchandarshekhar742@gmail.com>
2026-01-05 22:52:31 +0530

    Fix AndroidDevices api import - use default import instead of named import

--- frontend/src/pages/AndroidDevices.jsx
+++ frontend/src/pages/AndroidDevices.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
-import { api } from '../utils/api';
+import api from '../utils/api';
 import { toast } from 'react-hot-toast';
 
 const AndroidDevices = () => {
